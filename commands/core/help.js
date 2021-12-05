93c1960aaac662ed1bb0e078e32e912e9a7a6415
Umut Bayraktar <umutkarayilan55@hotmail.com>
2021-12-05 21:03:10 +0300

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'help',
-    aliases: ['h'],
+    aliases: ['h',"yardım","y"],
     showHelp: false,
     utilisation: '{prefix}help',
 
